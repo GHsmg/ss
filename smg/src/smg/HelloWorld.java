695c8bbc61991898690be79f08792049ae1bacf7
GHsmg <22733076562@qq.com>
2016-11-13 16:34:55 +0800

    提交文件

---
+++ smg/src/smg/HelloWorld.java
@@ -0,0 +1,13 @@
+package smg;
+
+public class HelloWorld {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		String love = "我爱慕课网";
+		String james = "www.imooc.com";
+		System.out.println(love);
+		System.out.println(james);
+	}
+
+}
